5e705483cce456e8c61995387188fbe9917bd5ce
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-05-16 14:45:34 +0700

    Feature: Add cors approval

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 
 const errorController = require("./controllers/errorController");
 const router = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use((req, res, next) => {
   req.requestTime = new Date();
