28a53d11e9f2a38d16276e1d45b60b31cca45397
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-05-23 10:22:43 +0700

    rollback

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
-const { Sequelize } = require("sequelize");
 
 const errorController = require("./controllers/errorController");
 const router = require("./routes");
 const cors = require("cors");
 
 const app = express();
-
-const sequelize = new Sequelize(
-  "postgres://postgres.vrkqqfpvkdpbsmsbajjt:5oio1ff7e8fLQKwn@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres",
-  {
-    dialectModule: require("pg"),
-  }
-);
 
 // Using middleware
 app.use(express.json());
